8b4dfc98c18cf7005a721acfb3182bc700888830
tangens90-web <mitro-tema@mail.ru>
2025-06-24 13:42:04 +0300

    добавили deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+
+
+set -e
+
+
+git pull origin/main
+
+php8.1 composer.phar install --no-dev --optimize-autoloader
+
+php8.1 artisan migrate --force
+
+php8.1 artisan event:cache
+
+php8.1 artisan view:cache
+php8.1 artisan route:cache
